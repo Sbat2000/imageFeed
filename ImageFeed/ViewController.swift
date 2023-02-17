16dec4f1324754742cecd85e855836ecd3962a42
Sbat2000 <103517615+Sbat2000@users.noreply.github.com>
2023-02-17 22:34:42 +0800

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Александр Гарипов on 17.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

6962b6d9a2c0075cd281fe03f8a5ede7395724b9
Sbat2000 <zevzok@gmail.com>
2023-02-20 21:36:23 +0800

    add gradientView

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -6,29 +6,30 @@ final class ImagesListCell: UITableViewCell {
     @IBOutlet weak var cellImage: UIImageView!
     @IBOutlet weak var cellLikeButton: UIButton!
     @IBOutlet weak var cellDateLabel: UILabel!
+    @IBOutlet weak var gradientView: UIView!
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        cellImage.layer.sublayers = nil
+        gradientView.layer.sublayers = nil
         setupGradient()
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
-        cellImage.layer.sublayers = nil
+        gradientView.layer.sublayers = nil
     }
     
     func setupGradient() {
-        let height = bounds.height
-        let width = bounds.width
-        let heightGradient: CGFloat = 30
-        
         let colorTop = UIColor(red: 0.1, green: 0.11, blue: 0.13, alpha: 0.0).cgColor
         let colorBot = UIColor(red: 0.1, green: 0.11, blue: 0.13, alpha: 1.0).cgColor
+        
         let gradientLayer = CAGradientLayer()
-        gradientLayer.frame = CGRect(x: 0, y: height - heightGradient, width: width, height: heightGradient)
+        gradientLayer.frame = gradientView.bounds
+        gradientLayer.cornerRadius = cellImage.layer.cornerRadius
+        gradientLayer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
         gradientLayer.colors = [colorTop, colorBot]
-        cellImage.layer.insertSublayer(gradientLayer, at: 0)
+        gradientLayer.locations = [0, 1]
+        gradientView.layer.insertSublayer(gradientLayer, at: 0)
     }
     
 }

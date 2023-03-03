ad1c5ce638a4561d14c31ba8ea5a2e1bc377b948
Sbat2000 <zevzok@gmail.com>
2023-03-03 23:25:59 +0800

    add profileView

---
+++ ImageFeed/ImagesList/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Aleksandr Garipov on 03.03.2023.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+

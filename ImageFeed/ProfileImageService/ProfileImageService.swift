2261872c620a6c05bb60af6d2f1aea586ff001fa
Sbat2000 <zevzok@gmail.com>
2023-04-08 22:56:29 +0800

    add porfileimageservice

---
+++ ImageFeed/ProfileImageService/ProfileImageService.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileImageService.swift
+//  ImageFeed
+//
+//  Created by Aleksandr Garipov on 08.04.2023.
+//
+
+import Foundation
---
+++ ImageFeed/ProfileImageService/UserResult.swift
@@ -0,0 +1,8 @@
+//
+//  UserResult.swift
+//  ImageFeed
+//
+//  Created by Aleksandr Garipov on 08.04.2023.
+//
+
+import Foundation
